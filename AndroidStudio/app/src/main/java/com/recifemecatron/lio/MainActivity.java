29fab2601dfa809349e086b8e818e43aee603d8a
recifemecatron <45640119+recifemecatron@users.noreply.github.com>
2020-07-31 09:08:13 -0300

    Avanço na documentação LIO
    Criação das credenciais e order manager

--- AndroidStudio/app/src/main/java/com/recifemecatron/lio/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/recifemecatron/lio/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import cielo.orders.domain.Credentials;
+import cielo.sdk.order.OrderManager;
+import cielo.sdk.order.ServiceBindListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Credentials credenciais = new Credentials("Seu client id", "Seu accessToken aqui");
+
+        OrderManager orderManager = new OrderManager(credenciais, MainActivity.this);
+
+        orderManager.bind(MainActivity.this, serviceBindListener);
     }
+
+    ServiceBindListener serviceBindListener = new ServiceBindListener() {
+
+        @Override public void onServiceBoundError(Throwable throwable) {
+            //Ocorreu um erro ao tentar se conectar com o serviço OrderManager
+        }
+
+        @Override
+        public void onServiceBound() {
+            //Você deve garantir que sua aplicação se conectou com a LIO a partir desse listener
+            //A partir desse momento você pode utilizar as funções do OrderManager, caso contrário uma exceção será lançada.
+        }
+
+        @Override
+        public void onServiceUnbound() {
+            // O serviço foi desvinculado
+        }
+    };
 }
